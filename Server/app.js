a36ff53edd8d82bb4654103606e6af8e1275e821
Aditya <adityakumar4176o@gmail.com>
2025-12-25 04:05:58 +0530

    auth done on basic level

--- Server/app.js
+++ Server/app.js
@@ -1,11 +1,12 @@
 import express from 'express';
-
+import userRouter from './routes/User.js';
 const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
+app.use('/user', userRouter);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
---
+++ Server/routes/User.js
@@ -0,0 +1,48 @@
+import express from "express";
+import bcrypt from "bcryptjs";
+import User from "../models/User.js";
+import jwt from "jsonwebtoken";
+import { auth } from "../middleware/auth.js";
+const router = express.Router();
+
+router.post("/register", async (req, res) => {
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = new User({
+      username,
+      password: hashedPassword,
+    });
+    await newUser.save();
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (err) {
+    console.error("Registration error:", err);
+    res.status(500).json({ message: "Server error" });
+  }
+});
+router.post("/login", async (req, res) => {
+    const{username,password}=req.body;
+    if(!username || !password){
+        return res.status(400).json({message:"Username and password are required"});
+    }
+    try {
+        const existingUser = await User.findOne({ username });
+        if (!existingUser) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const token = jwt.sign({ id: existingUser._id }, process.env.TOKEN_SECRET, { expiresIn: "1h" });
+        res.json({ token });
+    } catch (error) {
+        console.log("Login error:", error);
+        res.status(500).json({ message: "Server error" });
+    }});
+
+
+export default router;
