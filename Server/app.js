e9f8fec26a321d685ea6a054322d261d44861164
Aditya <adityakumar4176o@gmail.com>
2025-12-25 03:20:32 +0530

    Fix API endpoint path

--- Server/app.js
+++ Server/app.js
@@ -1,4 +1,4 @@
-import exprss from 'express';
+import express from 'express';
 
 const app = express();
 const PORT = 3000;
@@ -6,7 +6,6 @@ const PORT = 3000;
 app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
-
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
